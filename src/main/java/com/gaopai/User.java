39b5f31f6d3caeff4ce008040133dabd9e89cabe
gaopai <494209943@qq.com>
2017-11-18 15:38:42 +0800

    修改

--- src/main/java/com/gaopai/User.java
+++ src/main/java/com/gaopai/User.java
@@ -4,4 +4,11 @@ public class User {
 private int age;
 private String name;
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
