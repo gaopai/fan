1f04ba2ea4716e32ab43466a9ef7056b716d2a13
bianziyi <2294668139@qq.com>
2017-11-18 16:02:26 +0800

    2222222222222222222

--- src/main/java/com/gaopai/User.java
+++ src/main/java/com/gaopai/User.java
@@ -4,6 +4,13 @@ public class User {
     private int age;
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public int getAge() {
         return age;
